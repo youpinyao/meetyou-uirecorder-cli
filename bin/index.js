7e5f7b44087bc324e1e0d79140e2a28c1a17636e
游品尧 <497400448@qq.com>
2018-02-11 15:45:25 +0800

    readme

---
+++ bin/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const commander = require('commander');
+const init = require('./src/init');
+const record = require('./src/record');
+const run = require('./src/run');
+const server = require('./src/server');
+
+commander
+  .version(require('./package.json').version)
+  .option('init', 'init test project', function () {
+    init();
+  })
+  .option('record <filePath>', 'start a record', function (filePath) {
+    record(filePath);
+  })
+  .option('server', 'start selenium-standalone server', function () {
+    server();
+  })
+  .option('run <filePath>', 'run a test', function (filePath) {
+    run(filePath);
+  })
+  .option('upgrade', 'upgrade version', function () {
+    upgrade();
+  })
+  .on('--help', function(){
+
+  })
+  .parse(process.argv);
+
+  // 默认输出帮助
+  if (!process.argv.slice(2).length) {
+    commander.outputHelp();
+  }
